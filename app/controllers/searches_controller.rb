bc6ff313791dba34b42a4eea7b3ffe3542b56701
Adina Bianchi <adina.r.bianchi@gmail.com>
2016-10-13 11:06:53 -0400

    form start

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,11 @@
 class SearchesController < ApplicationController
+  def new
+  end
+
   def create
+    source = Source.first_or_create(name: params[:source], url: params[:url])
+    Search.create(keyword: params[:keyword], source_id: source.id, user_id: current_user.id)
+    redirect_to :dashboard
   end
 
   def edit
